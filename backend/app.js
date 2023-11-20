3659ad7b50c4ebfb2f4506f6cb44583f2515a241
Mansiprajapatii <82796129+Mansiprajapatii@users.noreply.github.com>
2023-11-20 22:49:10 +0530

    schema added

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 require("dotenv").config();
 
+const User = require("./model/User");
+
 const app = express();
-app.use(express.json); //  middleware to read the data in json format as it by default don't do that
+app.use(express.json()); //  middleware to read the data in json format as  express by default don't do that
 
 app.get("/", (req, res) => {
   res.send("<h1>Hello from auth system</h1>");
@@ -10,6 +12,18 @@ app.get("/", (req, res) => {
 
 app.post("/register", (req, res) => {
   const { firstname, lastname, email, password } = req.body;
+
+  if (!(firstname && lastname && email && password)) {
+    res.status(400).send("All fields are mandatroy");
+  }
+
+  // to check existing user we use the query
+
+  const existingUser = User.findOne({ email });
+
+  if (existingUser) {
+    res.status(401).send("User already exists");
+  }
 });
 
 module.exports = app;
